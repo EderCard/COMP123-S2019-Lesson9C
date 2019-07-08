97b19d49ec632d775c9dfd2bc2b3abcb432d08c8
EderCard <edersoncard@gmail.com>
2019-07-08 16:28:37 -0400

    Add event handler to buttons

--- COMP123-S2019-Lesson9C/CalculatorForm.cs
+++ COMP123-S2019-Lesson9C/CalculatorForm.cs
@@ -12,9 +12,33 @@ namespace COMP123_S2019_Lesson9C
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// This is the constructor for the calculator
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the event handler for the CalculatorButton click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+
+            int ButtonValue;
+            bool Result = int.TryParse(TheButton.Text, out ButtonValue);
+
+            if (Result)
+            {
+                ResultLabel.Text = TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "Not a number (NAN)";
+            }
         }
     }
 }
